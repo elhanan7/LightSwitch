71029513e1e5d8d4d0a29f2f4442f683851e0c11
Elhanan Ilani <elhanan.ilani@gmail.com>
2015-10-27 00:12:44 +0200

    Test GIT

--- app/src/main/java/com/example/elhanan7/lightswitch/LightSwitchMainActivity.java
+++ app/src/main/java/com/example/elhanan7/lightswitch/LightSwitchMainActivity.java
@@ -9,5 +9,6 @@ public class LightSwitchMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light_switch_main);
+        // test
     }
 }
