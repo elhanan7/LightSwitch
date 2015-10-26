d2ad581891249fe38b374cc8188f3cb853f40daf
Elhanan Ilani <elhanan.ilani@gmail.com>
2015-10-27 00:02:52 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/elhanan7/lightswitch/LightSwitchMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.elhanan7.lightswitch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LightSwitchMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_light_switch_main);
+    }
+}
