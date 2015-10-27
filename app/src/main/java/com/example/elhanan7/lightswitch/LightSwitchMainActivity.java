e6b01f3b683fe63237d4dcf1c6417c1de60def89
Elhanan Ilani <elhanan.ilani@gmail.com>
2015-10-27 17:43:10 +0200

    Added some code

--- app/src/main/java/com/example/elhanan7/lightswitch/LightSwitchMainActivity.java
+++ app/src/main/java/com/example/elhanan7/lightswitch/LightSwitchMainActivity.java
@@ -3,7 +3,43 @@ package com.example.elhanan7.lightswitch;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class LightSwitchMainActivity extends AppCompatActivity {
+
+    final String USER_AGENT = "Mozilla/5.0";
+
+    private int getCurrentValue() throws Exception {
+        URL url = new URL("http://10.0.0.6:8080/get");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("User-Agent", USER_AGENT);
+        int responseCode = connection.getResponseCode();
+        if (responseCode == 404) {
+            return -1;
+        }
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine = in.readLine();
+        if (inputLine.equals("OFF")) {
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
+
+    private void setValue(int state) throws Exception {
+        URL url = new URL("http://10.0.0.6:8080/set/" + (state == 0? "off" : "on"));
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("User-Agent", USER_AGENT);
+        int responseCode = connection.getResponseCode();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
